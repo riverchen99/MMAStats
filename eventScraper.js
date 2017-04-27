c862969482f59c9788da3f7d71331fe676e72482
Chuan Chen <riverchen@gmail.com>
2017-04-27 15:17:24 -0400

    Fixed scraping for all result cases

--- eventScraper.js
+++ eventScraper.js
@@ -38,15 +38,25 @@ function processEventPage(callback) {
 			console.log("Location: " + $("span[itemprop = 'location']").text());
 			*/
 
-			// have to fix no contest
 			fights = [];
+			method_text = $("table.resume td").eq(1).text().substring(7);
+			if (method_text.includes(" (")) {
+				main_method = method_text.split(" (")[0];
+				sub_method = method_text.split(" (")[1].slice(0, -1);
+			} else if (method_text.includes(" - ")) {
+				main_method = method_text.split(" - ")[0];
+				sub_method = method_text.eq(1).text().substring(7).split(" - ")[1];
+			} else {
+				main_method = method_text.eq(1).text().substring(7);
+				sub_method = "N/A";
+			}
 			fights.push({
 				"main": true,
 				"match": parseInt($("table.resume td").eq(0).text().split(" ")[1]),
 				"winner": $("div.fighter.left_side").children().find("span[itemprop = 'name']").text(),
 				"loser": $("div.fighter.right_side").children().find("span[itemprop = 'name']").text(),
-				"main_method": $("table.resume td").eq(1).text().substring(7, 17) == "No Contest" ? "No Contest" : $("table.resume td").eq(1).text().substring(7).split(" (")[0],
-				"sub_method": $("table.resume td").eq(1).text().substring(7, 17) == "No Contest" ? "No Contest" : $("table.resume td").eq(1).text().substring(7).split(" (")[1].slice(0, -1),
+				"main_method": main_method,
+				"sub_method": sub_method,
 				"referee": $("table.resume td").eq(2).text().substring(8),
 				"round": parseInt($("table.resume td").eq(3).text().split(" ")[1]),
 				"min": parseInt($("table.resume td").eq(4).text().split(" ")[1].split(":")[0]),
@@ -54,13 +64,24 @@ function processEventPage(callback) {
 			})
 
 			$("tr[itemprop = 'subEvent']").each(function (index) {
+				method_text = $(this).children().eq(4).contents().get(0).nodeValue;
+				if (method_text.includes(" (")) {
+					main_method = method_text.split(" (")[0];
+					sub_method = method_text.split(" (")[1].slice(0, -1);
+				} else if (method_text.includes(" - ")) {
+					main_method = method_text.split(" - ")[0];
+					sub_method = method_text.split(" - ")[1];
+				} else {
+					main_method = method_text;
+					sub_method = "N/A";
+				}
 				fights.push({
 					"main": false,
 					"match": parseInt($(this).children().eq(0).text().trim()),
 					"winner": $(this).children().find("td.text_right.col_fc_upcoming span[itemprop = 'name']").text(),
 					"loser": $(this).children().find("td.text_left.col_fc_upcoming span[itemprop = 'name']").text(),
-					"main_method": $(this).children().eq(4).contents().get(0).nodeValue.substring(0, 10) == "No Contest" ? "No Contest" : $(this).children().eq(4).contents().get(0).nodeValue.split(" (")[0],
-					"sub_method": $(this).children().eq(4).contents().get(0).nodeValue.substring(0, 10) == "No Contest" ? null : $(this).children().eq(4).contents().get(0).nodeValue.split(" (")[1].slice(0, -1),
+					"main_method": main_method,
+					"sub_method": sub_method,
 					"referee": $(this).children().find("span.sub_line").text(),
 					"round": parseInt($(this).children().eq(5).text()),
 					"min": parseInt($(this).children().eq(6).text().split(":")[0]),
@@ -73,7 +94,7 @@ function processEventPage(callback) {
 				"organization": $("div.section_title h2 span").text(),
 				"date": $("meta[itemprop = 'startDate']").attr("content").split("T")[0],
 				"time": $("meta[itemprop = 'startDate']").attr("content").split("T")[1],
-				"location": "Location: " + $("span[itemprop = 'location']").text(),
+				"location": $("span[itemprop = 'location']").text(),
 				"fights": fights,
 			});
 
